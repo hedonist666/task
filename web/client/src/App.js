6fd3fb9634217c018fd776c4cf9446bd9e71e209
Your Name <you@example.com>
2021-01-27 02:56:23 +0300

    tmp commit

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -8,36 +8,77 @@ class Header extends React.Component {
     render() {
         return (
             <header className="main-header">
-            <div class="links">
+            <div className="links">
                 <img src={rosneft_logo} className="logo-img" alt="logo" />
                 <img src={hexagon1} className="hexagon1" alt="hexagon" />
                 <img src={hexagon2} className="hexagon2" alt="hexagon" />
-                <a class="header-link" href="#" rel="noopener noreferrer">Главная</a>
-                <a class="header-link" href="#" rel="noopener noreferrer">Организаторы</a>
-                <a class="header-link" href="#" rel="noopener noreferrer">Правила</a>
-                <a class="header-link" href="#" rel="noopener noreferrer">#ПульсМарафон</a>
-                <a class="header-link" href="#" rel="noopener noreferrer" style={{paddingRight: '40px'}}>rn.digital</a>
+                <a className="header-link" href="#" rel="noopener noreferrer">Главная</a>
+                <a className="header-link" href="#" rel="noopener noreferrer">Организаторы</a>
+                <a className="header-link" href="#" rel="noopener noreferrer">Правила</a>
+                <a className="header-link" href="#" rel="noopener noreferrer">#ПульсМарафон</a>
+                <a className="header-link" href="#" rel="noopener noreferrer" style={{paddingRight: '40px'}}>rn.digital</a>
             </div>
             </header>
         )
     }
 }
 
-
-function Button(props) {
+function News(props) {
     return (
-        <button className="rect-btn">
-            {props.text}
-        </button>
+        <div class="news-block">
+            <div class="new-header">
+                {props.header}
+            </div>
+            <div class="news-content">
+                {props.content}
+            </div>
+            <a class="read-further" href={props.link}>
+                Читать дальше
+            </a>
+        </div>
     )
 }
+
+class Marathon extends React.Component {
+    render() {
+        return (
+            <div className="marathon">
+                <div className="main-txt">
+                    Марафон <br /> ИТ-соревнований
+                </div>
+                <div className="sub-txt">
+                    Роснефть приглашает разработчиков и аналитиков принять участие в одном из трёх соревнований
+                </div>
+                <div class="news">
+                    <div class="news-header">
+                        #ПульсМарафон
+                    </div>
+                    <News 
+                        header="Отложение высвобождает пегматитовый сталагмит"
+                        content="Базис эрозии, основываясь большей частью на сейсмических данных, глобален. Эоловое засоление ослабляет комплекс.  Лагуна, так же, как и в других..."
+                        link="#"
+                    />
+                    <News 
+                        header="Плато смещает аллит, что, однако, не уничтожило доледниковую"
+                        content="Пока магма остается в камере, углефикация сдвигает меловой мусковит. Вулканическое стекло, скажем, за 100 тысяч лет, сменяет глетчерный приток, причем, вероятно..."
+                        link="#"
+                    />
+                    <a class="to-new" href="#">Ко всем новостям</a>
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+
 
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{height: '100%'}}>
                 <Header />
-                <Button text="test" />
+                <Marathon />
             </div>
         )
     }
