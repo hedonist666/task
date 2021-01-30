24b3a6de087e25631a9323306b9a99b1f8e8038b
Your Name <you@example.com>
2021-01-30 23:28:15 +0300

    minor changes

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -191,35 +191,6 @@ class GradientButton extends React.Component {
     }
 }
 
-/*
-const fadeIn = keyframes`from { opacity: 0; } to { opacity: 1; }`;
-const FadeIn = ({ 
-    duration = 300,
-    delay = 0,
-    children,
-    ...delegated 
-}) => {
-    return ( 
-        <Wrapper 
-            {...delegated} 
-            style= {{ 
-                ...(delegated.style || {}), 
-                animationDuration: duration + 'ms', 
-                animationDelay: delay + 'ms', 
-            }} 
-        > 
-            {children} 
-        </Wrapper>
-    );
-};
-
-const Wrapper = styled.div`
-    @media (prefers-reduced-motion: no-preference) {
-        animation-name: $ {fadeIn};
-        animation-fill-mode: backwards;
-    } 
-`;
-*/
 
 function Annonce(props) {
     return(
